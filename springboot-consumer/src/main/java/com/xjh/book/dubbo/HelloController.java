2f077df3034e6b982661177dc476da719e018fe3
xujiahui <953323966@qq.com>
2021-09-12 14:43:03 +0800

    threadlocal

--- springboot-consumer/src/main/java/com/xjh/book/dubbo/HelloController.java
+++ springboot-consumer/src/main/java/com/xjh/book/dubbo/HelloController.java
@@ -4,6 +4,8 @@ import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+
 /**
  * @author xujiahui
  * @create 2021/7/25 17:05
@@ -14,6 +16,13 @@ public class HelloController {
 
 //    @Reference(url = "zookeeper://192.168.137.133:2181/com.xjh.book.dubbo.IHelloService")
     private IHelloService iHelloService;
+
+    private static final ThreadLocal<DateFormat> local = new ThreadLocal<>(){
+        @Override
+        protected DateFormat initialValue() {
+            return super.initialValue();
+        }
+    };
 
     @RequestMapping("/say")
     public String sayHello() {
