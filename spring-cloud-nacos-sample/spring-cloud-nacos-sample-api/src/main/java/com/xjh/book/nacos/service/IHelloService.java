13edd9283140f5720c8894d13853ca964a7206b0
xujiahui <953323966@qq.com>
2021-09-12 21:57:11 +0800

    修改注释

--- spring-cloud-nacos-sample/spring-cloud-nacos-sample-api/src/main/java/com/xjh/book/nacos/service/IHelloService.java
+++ spring-cloud-nacos-sample/spring-cloud-nacos-sample-api/src/main/java/com/xjh/book/nacos/service/IHelloService.java
@@ -2,7 +2,7 @@ package com.xjh.book.nacos.service;
 
 /**
  * @author user
- * @Description 这是develop分支
+ * @Description 这是master分支
  * @create 2021/8/11 22:49
  */
 public interface IHelloService {
